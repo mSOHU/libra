d3091b656b30326371d2f5670bcd18efd9ceed35
guangmingchen <guangmingchen@sohu-inc.com>
2015-05-12 17:44:20 +0800

    增加setup

--- setup.py
+++ setup.py
@@ -1,2 +1,34 @@
 #coding: utf-8
-__author__ = 'sohume'
+from setuptools import setup, find_packages
+
+PACKAGE = "libra"
+NAME = "libra"
+DESCRIPTION = "Load balancing algorithm"
+AUTHOR = "xuweizheng; ctycheer"
+AUTHOR_EMAIL = "xuweizheng@sohu-inc.com"
+URL = "http://m.sohu.com/"
+
+
+setup(
+    name=NAME,
+    version=0.1,
+    description=DESCRIPTION,
+    long_description="Load balancing",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    packages=find_packages(exclude=["*.pyc"]),
+    install_requires=[
+
+    ],
+    classifiers=[
+        "Topic: Load balancing algorithm",
+        "Operating System :: Ubuntu",
+        "Programming Language :: Python",
+
+    ],
+
+    # entry_points={
+    # },
+
+)
