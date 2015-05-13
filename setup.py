0d752631534a21a913c17c408ccc9c550bf003a7
guangmingchen <guangmingchen@sohu-inc.com>
2015-05-13 18:13:32 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ URL = "http://m.sohu.com/"
 
 setup(
     name=NAME,
-    version='0.0.3',
+    version='0.0.4',
     description=DESCRIPTION,
     long_description="Load balancing",
     author=AUTHOR,
