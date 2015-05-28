fb5c43b94024c7e6d18548e6ede114888ea2412a
johnxu <weizhengxu@sohu-inc.com>
2015-05-28 10:43:54 +0800

    - add log  - fix initialize progress

--- libra/adapters/tornado_http.py
+++ libra/adapters/tornado_http.py
@@ -2,31 +2,38 @@
 
 __author__ = 'johnxu'
 
+import logging
 import functools
 
 from tornado import simple_httpclient
 
 
-class LibraAsyncHTTPClient(simple_httpclient.SimpleAsyncHTTPClient):
-    def __init__(self, manager, placeholder='__node__'):
-        self.manager = manager
-        self.placeholder = placeholder
+LOGGER = logging.getLogger(__name__)
 
+
+class LibraAsyncHTTPClient(simple_httpclient.SimpleAsyncHTTPClient):
     def initialize(self, io_loop=None, max_clients=10,
-                   hostname_mapping=None, max_buffer_size=104857600):
+                   hostname_mapping=None, max_buffer_size=104857600,
+                   manager=None, placeholder='__node__'):
         super(LibraAsyncHTTPClient, self).initialize(
             io_loop=io_loop, max_clients=10,
             hostname_mapping=None, max_buffer_size=104857600)
+
+        self.manager = manager
+        self.placeholder = placeholder
 
     def fetch(self, request, callback, **kwargs):
         @functools.wraps(callback)
         def wrapper(response):
             if response.error:
+                LOGGER.error('LIBRA: dead node, %s, %ss', node, response.request_time)
                 self.manager.dead_node(node, response.request_time)
             else:
+                LOGGER.debug('LIBRA: release node, %s, %ss', node, response.request_time)
                 self.manager.release_node(node, response.request_time)
             return callback(response)
 
         node = self.manager.get_node()
+        LOGGER.debug('LIBRA: got node, %s', node)
         request.url = request.url.replace(self.placeholder, node)
         return super(LibraAsyncHTTPClient, self).fetch(request, wrapper, **kwargs)
