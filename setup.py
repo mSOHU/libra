6eb5b5612beefb25e32a0134908945f7461628c7
boyxuper <boyxuper@gmail.com>
2016-01-27 13:34:22 +0800

    update to version 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libra',
-    version='0.1.2',
+    version='0.1.3',
     author='xuweizheng; ctycheer',
     author_email='xuweizheng@sohu-inc.com',
     url='http://m.sohu.com/',
