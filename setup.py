f779d87c82a3b33d7eaed6b6b77b72ffdc47077e
boyxuper <boyxuper@gmail.com>
2016-03-08 16:05:45 +0800

    bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libra',
-    version='0.2.1',
+    version='0.2.2',
     author='xuweizheng; ctycheer',
     author_email='xuweizheng@sohu-inc.com',
     url='http://m.sohu.com/',
